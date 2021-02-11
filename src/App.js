4ef64cfadb19135ebd47603f3419ca1134539eed
Louis Chang <hanchung.chang@oath.com>
2021-02-11 15:54:15 +0800

    navigator bar example

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import './App.css';
 import {
-  HomePage,
-  CounterButtonPage,
-  PeopleListPage,
-  NotFoundPage,
-  ProtectedPage,
   ControlledFormPage,
+  CounterButtonPage,
+  HomePage,
+  NotFoundPage,
+  PeopleListPage,
+  ProtectedPage,
   UnControlledFormPage
 } from './pages';
-import logo from './logo.svg';
-import { CounterButton } from './CounterButton'
-import { CongratulationsMessage } from './CongratulationsMessage';
-import { Greeting } from './Greeting';
-import { PeopleList } from './PeopleList';
-import './App.css';
+import { FormsNavBar } from './FormsNavBar';
+import { NavBar } from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/counter">Go to Counter Page</Link>
-        <Link to="/people-list">Go to PeopleList Page</Link>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/counter/:name">
-            <CounterButtonPage />
-          </Route>
-          <Route path="/people-list">
-            <PeopleListPage />
-          </Route>
-          <Route path="/protected">
-            <ProtectedPage />
-          </Route>
-          <Route path="/controlled">
-            <ControlledFormPage />
-          </Route>
-          <Route path="/uncontrolled">
-            <UnControlledFormPage />
-          </Route>
-          <Route>
-            <NotFoundPage />
-          </Route>
-        </Switch>
+        <NavBar />
+        <div className="App-header">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/counter">
+              <CounterButtonPage />
+            </Route>
+            <Route path="/people-list">
+              <PeopleListPage />
+            </Route>
+            <Route path="/protected">
+              <ProtectedPage />
+            </Route>
+            <Route path="/forms">
+              <FormsNavBar />
+              <Route path="/forms/controlled">
+                <ControlledFormPage />
+              </Route>
+              <Route path="/forms/uncontrolled">
+                <UnControlledFormPage />
+              </Route>
+            </Route>
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </div>
   );
